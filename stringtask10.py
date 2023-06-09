a9ed75fd8c1c0eb3764513fa109129a44c13fe0c
D-Tushkanov <tushkanovd@mail.ru>
2023-06-09 14:25:54 +0300

    Add files via upload

--- stringtask10.py
+++ stringtask10.py
@@ -1,5 +1,5 @@
 #input variable
-string = input("Enter your text: ")
+string = input('Enter your text: ')
 #second variable for index search and character substitution
 new_str = string[-1] + string[1:-1] + string[0]
 #output
