f75141b8d0371e24a483b7789aa74c9b60ac8081
D-Tushkanov <tushkanovd@mail.ru>
2023-06-09 14:29:37 +0300

    Add files via upload

--- stringtask10.py
+++ stringtask10.py
@@ -1,3 +1,6 @@
+"""Write a Python program to change a given string to a newly string where
+the first and last chars have been exchanged. """
+
 #input variable
 string = input('Enter your text: ')
 #second variable for index search and character substitution
