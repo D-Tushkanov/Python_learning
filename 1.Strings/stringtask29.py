2549daa6fe0792ef4bbd1b57da327ffe70b33977
D-Tushkanov <tushkanovd@mail.ru>
2023-07-03 09:33:17 +0300

    Add files via upload

---
+++ 1.Strings/stringtask29.py
@@ -0,0 +1,11 @@
+"""Write a Python program to set the indentation of the first line. """
+#first variable
+string = input("Enter your text: ")
+#second variable
+space = " "
+#combine strings
+modified_string = "".join([space, string])
+#output first variable
+print(string)
+#output combine strings
+print(modified_string)
