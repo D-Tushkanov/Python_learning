a02c505f63307624f9aeae1fdd8b0217e7e73016
D-Tushkanov <tushkanovd@mail.ru>
2023-07-03 10:09:40 +0300

    Add files via upload

--- 1.Strings/stringtask16.py
+++ 1.Strings/stringtask16.py
@@ -1,9 +1,13 @@
-string = input('Enter your text: ')
-new_string = string
-changed_string = modifield_string = 0
-changed_string = string.find('not')
-modifield_string = string.find('poor')
-print(changed_string, modifield_string)
-if (changed_string >= 0 and modifield_string >= 0):
-    new_string = new_string.replace(new_string[changed_string:modifield_string],'good', 4)
-print(new_string)
+"""Write a Python function to insert a string in the middle of a string.
+Sample function and result :
+insert_sting_middle('[[]]<<>>', 'Python') -> [[Python]]
+insert_sting_middle('{{}}', 'PHP') -> {{PHP}}"""
+#first variable
+string = 'Python'
+#second variable
+new_string = '[[]]'
+#the concatenation variable of the first and second variables
+modified_string = new_string[:2] + string + new_string[2:]
+#output
+print(modified_string)
+
